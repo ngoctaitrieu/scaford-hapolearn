b9fe0f2e9fbaa7795aaeb408b1551ad9e00fd3d0
trieu ngoc tai <ngoctai28112k@gmail.com>
2022-06-16 13:01:51 +0700

    upload scaford-hapolearn

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<header class="main-header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto w-100">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ALL COURSES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" aria-haspopup="true" aria-expanded="false">
+                        LOGIN/REGISTER
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PROFILE</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
