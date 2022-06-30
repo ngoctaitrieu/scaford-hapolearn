dd8f37badf65d25228561c2dde91b2aae82bf16c
trieu ngoc tai <ngoctai28112k@gmail.com>
2022-06-30 10:41:34 +0700

    fix EOF in slinkk.js

--- resources/js/slickk.js
+++ resources/js/slickk.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
-    $('.feedback-list').slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      prevArrow:"<button type='button' class='slick-prev'><i class='fa-solid fa-angle-left'></i></button>",
-      nextArrow:"<button type='button' class='slick-next'><i class='fa-solid fa-angle-right'></i></button>",
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
+  $('.feedback-list').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow:"<button type='button' class='slick-prev'><i class='fa-solid fa-angle-left'></i></button>",
+    nextArrow:"<button type='button' class='slick-next'><i class='fa-solid fa-angle-right'></i></button>",
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
       {
-      breakpoint: 1024,
-      settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      infinite: true,
-          }
-        },
-        {
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        }
+      },
+      {
         breakpoint: 739,
         settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
-            }
-          },
-        ]
-      });
-    });
+        }
+      },
+    ]
+  });
+});
