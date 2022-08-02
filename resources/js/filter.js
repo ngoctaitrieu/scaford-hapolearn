a76f9e8fa4db5389dd89dc37b5522bd1bab60ed6
trieu ngoc tai <ngoctai28112k@gmail.com>
2022-08-02 10:44:37 +0700

    use jquery in filter

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,6 +1,5 @@
-const filterBtn = document.querySelector('.filter-btn');
-const filterContent = document.querySelector('.filter-content');
-
-filterBtn.addEventListener('click', ()=> {
-  filterContent.classList.toggle("filter-show");
+$(function() {
+  $('.filter-btn').click(function() {
+    $('.filter-content').toggleClass('filter-show');
+  });
 });
